59db3d99488792a248d8d069ecc2bed70c0e9a35
orazi-paolo <orazi.paolo10@gmail.com>
2024-09-19 15:39:53 +0200

    added markup in card list & card list item

--- src/components/AppMainCardsList.vue
+++ src/components/AppMainCardsList.vue
@@ -13,9 +13,19 @@ export default {
 </script>
 
 <template>
-    <AppMainCardsListItem />
+  <div class="container bg-light p-5">
+    <div class="bg-black pt-3 pb-3 d-flex align-items-center">
+      <p class="results mb-0 ps-3">Found x cards</p>
+    </div>
+    <div class="row row-cols-5">
+      <AppMainCardsListItem />
+    </div>
+  </div>
 
 </template>
 
 <style lang="scss" scoped>
+.results{
+  color: white;
+}
 </style>
--- src/components/AppMainCardsListItem.vue
+++ src/components/AppMainCardsListItem.vue
@@ -9,7 +9,20 @@ export default {
 </script>
 
 <template>
+    <div class="card border-0">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title text-center">Titolo carta</h5>
+          <p class="card-text text-center">Tipologia carta</p>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+.card-body{
+  background-color: #d48f3c;
+  .card-title{
+    color: white;
+  }
+}
 </style>
